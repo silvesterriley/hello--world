15e7bbe882f81b77c5d9ff2e0212209078e03a12
silvester <43179117+silvesterriley@users.noreply.github.com>
2018-09-17 23:57:06 -0700

    Add files via upload

---
+++ code.py
@@ -0,0 +1,28 @@
+message="hello python"
+print(message)
+
+#variables to hold my names
+firstname="Silvester"
+middlename="Muthiri"
+lastname="Marubu"
+Age=24
+
+
+#i want to print my details
+print("My first name is", firstname)
+print("My middle name is" ,middlename)
+print("My last name is" ,lastname)
+print(Age,"years old")
+
+
+#printing all variables in one line
+print("My first name is {0} and My middle name is {1} and last name is {2} {3} years of age".format(firstname,middlename,lastname,Age))
+
+#printing all variables in one line using percentage
+print("My first name is %s and My middle name is %s and last name is %s %d years of age" %(firstname,middlename,lastname,Age))
+
+#simple calculation 
+a=6
+b=10
+total = a+b
+print(total)
---
+++ prgcd.py
@@ -0,0 +1,31 @@
+message="hello python"
+print(message)
+
+#variables to hold my names
+firstname="paloma"
+middlename="foster"
+lastname="kiragu"
+Age=24
+
+
+#i want to print my details
+print("My first name is", firstname)
+print("My middle name is" ,middlename)
+print("My last name is" ,lastname)
+print(Age,"years old")
+
+
+#printing all variables in one line
+print("My first name is {0} and My middle name is {1} and last name is {2} {3} years of age".format(firstname,middlename,lastname,Age))
+
+#printing all variables in one line using percentage
+print("My first name is %s and My middle name is %s and last name is %s %d years of age" %(firstname,middlename,lastname,Age))
+
+#simple calculation 
+#area of a circle
+
+
+
+
+
+
