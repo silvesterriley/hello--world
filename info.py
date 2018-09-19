5834a17e8d6a9a15924ecb670cbcdc33df22c475
silvesterry <silvsterry101@gmail.com>
2018-09-18 18:18:00 -0700

    updated assignment operators and comparison operators to info.py

--- info.py
+++ info.py
@@ -24,8 +24,26 @@ print("My first name is %s and My middle name is %s and last name is %s %d years
 #simple calculation 
 a=6
 b=10
-total = a+b
-print(total)
+c=690
+total = a+b+c
+average = total//3
+difference =c-a
+remainder =b%a
+print("the total of {0} + {1} + {2} is = {3}" .format(a,b,c, total))
+print("the difference btwn {0} - {1} is ={2}" .format(c, a, difference))
+print("the average of {0} + {1} + {2} is = {3}" .format(a,b,c, average))
+print("the remainder of {0} divided by {1}  is = {2}" .format(b, a, remainder))
+
+#test for largest
+if(a<b):
+    print("{0] is largest".format(b))
+else:
+    print("{0] is largest".format(a))
+
+
+
+    
+
 
 #Area of rectangle
 #calculate 
