2545f663cb747534126d7020fe3c27a0708445e5
silvesterry <silvsterry101@gmail.com>
2018-09-18 17:39:05 -0700

    added a new file called prgcd.py

--- prgcd.py
+++ prgcd.py
@@ -2,9 +2,9 @@ message="hello python"
 print(message)
 
 #variables to hold my names
-firstname="cassandra"
-middlename="Ndanu"
-lastname="Nduati"
+firstname="paloma"
+middlename="foster"
+lastname="kiragu"
 Age=24
 
 
@@ -22,18 +22,10 @@ print("My first name is {0} and My middle name is {1} and last name is {2} {3} y
 print("My first name is %s and My middle name is %s and last name is %s %d years of age" %(firstname,middlename,lastname,Age))
 
 #simple calculation 
-a=6
-b=10
-total = a+b
-print(total)
+#area of a circle
 
-#Area of rectangle
-#calculate 
-Length= 20
-width= 10
-area=(Length * width)
-perimeter=(Length + width)*2
 
-#output
-print("the length is = {0} and width is ={1} thus the area is={2}".format(Length,width, area))
-print("the length is %d and width is %d thus the area is %d thus be perimeter" %(Length,width,perimeter))
+
+
+
+
