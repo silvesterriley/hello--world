1ac2fb63ccc17014eab3ad7946a7c875b7333945
silvesterry <silvsterry101@gmail.com>
2018-09-26 19:21:00 -0700

    added a file of dy.py and updated sp.py

---
+++ dy.py
@@ -0,0 +1,10 @@
+   
+days=["monday","tuesday","wednesday","thursday","friday"]
+
+for day in days:
+    print(day.capitalize())
+    
+
+for x in range(1,11):
+    print("{0:2d} {1:3d} {2:4d}" .format(x,x*x,x*x*x*x))
+    
--- sp.py
+++ sp.py
@@ -6,4 +6,13 @@ for letter in myName:
 listitems=["mangoes","fruits","bananas","apples","kiwi"]
 
 for fruit in listitems:
-    print(fruit)
+    print(fruit.capitalize())
+    print(fruit.upper())
+
+for x in range(1,101):
+    if(x%2==1):
+        print(x, end=" ")
+print("\n")
+for x in range(2,101):
+    if(x%2==0):
+        print(x,end=" ")
